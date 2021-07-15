6ef8de688c78156c0a4207509863ff3ce66d86d9
CadisRaziel <vitorcsharp@hotmail.com>
2021-07-15 15:55:45 -0300

    implementando a primeira etapa da camera

--- lib/modules/screen/camera_screen.dart
+++ lib/modules/screen/camera_screen.dart
@@ -1,21 +1,105 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 //*Colocando uma variavel aqui ela fica global para usarmos em qualquer lugar(variavel global)
+//*vai colcoar todas as cameras do dispotivo disponivel em uma lista
 List<CameraDescription>? cameras;
 
 class CameraScreen extends StatefulWidget {
-  const CameraScreen({ Key? key }) : super(key: key);
+  const CameraScreen({Key? key}) : super(key: key);
 
   @override
   _CameraScreenState createState() => _CameraScreenState();
 }
 
 class _CameraScreenState extends State<CameraScreen> {
+  CameraController? _cameraController;
+
+  Future<void>? cameraValue;
+
+  //*precisamos inicializar as daus variaveis '_cameraController' e 'cameraValue' no initState
+  @override
+  void initState() {
+    super.initState();
+    _cameraController = CameraController(cameras![0], ResolutionPreset.high);
+    cameraValue = _cameraController!.initialize();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Stack(
+        children: [
+          //*FutureBuilder para podermos acessar a camera
+          FutureBuilder(
+              future: cameraValue,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  return CameraPreview(_cameraController!);
+                } else {
+                  return Center(
+                      //*spinkit = CircularProgressindicator personalizado
+                      child: SpinKitThreeBounce(
+                    color: Colors.teal,
+                    size: 30.0,
+                  ));
+                }
+              }),
+          Positioned(
+            bottom: 0.0,
+            child: Container(
+              color: Colors.black,
+              padding: EdgeInsets.only(top: 5, bottom: 5),
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      IconButton(
+                        icon: Icon(
+                          Icons.flash_off,
+                          color: Colors.white,
+                          size: 28,
+                        ),
+                        onPressed: () {},
+                      ),
+                      InkWell(
+                        onTap: () {},
+                        child: Icon(
+                          Icons.panorama_fish_eye,
+                          color: Colors.white,
+                          size: 70,
+                        ),
+                      ),
+                      IconButton(
+                        icon: Icon(
+                          Icons.flip_camera_ios,
+                          color: Colors.white,
+                          size: 28,
+                        ),
+                        onPressed: () {},
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 4,
+                  ),
+                  Text(
+                    'Segure para video, clique para foto',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
